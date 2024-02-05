e8949fcbb585152613a4416992aa695e8415e4b7
DESKTOP-2SHLKQ2\dino <dino@btnet.com.tw>
2024-02-05 11:32:37 +0800

    fix: 更新api url

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const authorization = process.env.authorization || require('../config').authoriz
 
 module.exports = {
   GET_TIDAL_BY_DATE: (fromDate, toDate) => {
-    return `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-A0021-001?Authorization=${authorization}&format=JSON&sort=Date&timeFrom=${fromDate}&timeTo=${toDate}`;
+    return `https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-A0021-001?Authorization=${authorization}&format=JSON&sort=Date&timeFrom=${fromDate}&timeTo=${toDate}`;
   },
 
 }
